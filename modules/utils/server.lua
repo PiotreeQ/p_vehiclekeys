8ee9814198b61ae8cae72ca858f26a9886f26586
pScripts <47689001+PiotreeQ@users.noreply.github.com>
2026-06-22 14:45:00 +0200

    Update server.lua

--- modules/utils/server.lua
+++ modules/utils/server.lua
@@ -1,6 +1,7 @@
 local Utils = {}
 
 lib.locale(Bridge?.Config?.Language or 'en')
+lib.versionCheck('PiotreeQ/p_vehiclekeys')
 
 function Utils:trim(str)
     if type(str) ~= 'string' then return str end
